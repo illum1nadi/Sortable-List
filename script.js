349df467b2fb1dc889b241a01b4e8e2eaf8730b7
illum1nadi <adityainduri@gmail.com>
2025-05-24 15:28:02 +0530

    Dynamically loaded the shift content and made the shuffle function.

---
+++ script.js
@@ -0,0 +1,52 @@
+const draggableList = document.getElementById('draggableList');
+const submitButton = document.getElementById('submitButton');
+
+const list = [
+    "Jeff Bezos",
+    "Bill Gates",
+    "Warren Buffett",
+    "Bernard Arnault",
+    "Carlos Slim Helu",
+    "Amancio Ortega",
+    "Larry Ellison",
+    "Mark Zuckerberg",
+    "Micheal Bloomberg",
+    "Larry Page",
+];
+
+let currentList = list;
+
+function shuffleArray(array) {
+    //from gfg, code for fisher-yates shuffle algorithm.
+    for (let i = array.length - 1; i > 0; i--) { 
+    
+        // Generate random index 
+        const j = Math.floor(Math.random() * (i + 1));
+                    
+        // Swap elements at indices i and j
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+function startGame() {
+    currentList = shuffleArray(currentList);
+    currentList.forEach((item, index) => {
+        const listItem = document.createElement('li');
+        listItem.className = "flex justify-center";
+        listItem.innerHTML = `
+            <div class="flex justify-left items-center border-1 w-70 h-15">
+                <p class="w-14 h-14 text-center flex items-center justify-center bg-gray-300 mr-2">${index + 1}</p>
+                <div draggable="true" class="w-60 flex justify-between items-center">
+                    <div class="" >${item}</div>
+                    <span style="font-size: 24px;" class="mr-2">☰</span>
+                </div>
+            </div>
+        `
+        draggableList.appendChild(listItem);
+    })
+}
+
+startGame();
